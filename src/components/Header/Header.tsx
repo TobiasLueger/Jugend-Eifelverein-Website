405b5fb350805cade1049efeec8e060dc62c5edc
TobiasLueger <tobias.lueger98@gmail.com>
2022-05-29 10:57:51 +0200

    [TASK] make stage response and add header burger icon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -27,7 +27,7 @@ const Header = ({}) => {
 
 	return (
 		<header className="bg-white h-24  text-black flex items-center left-2/4 -translate-x-1/2 fixed w-11/12 rounded-b-3xl rounded-br-3xl justify-between p-[15px] z-[100]">
-			<div className="flex items-center">
+			<div className="flex items-center justify-between w-full">
 				<a href="/" className="flex items-center h-full mr-[50px]">
 					<img
 						src="https://jugend.eifel-53359.de/www/img/wier-logo.jpeg"
@@ -35,46 +35,54 @@ const Header = ({}) => {
 						className="w-[100px]"
 					/>
 				</a>
-				<nav>
-					<ul className="flex items-center justify-between h-full">
-						<li className="ml-12">
-							<Link className="font-lato font-bold text-[18px]" to="/about">
-								Über Uns
-							</Link>
-						</li>
-						<li className="ml-12">
-							<Link className="font-lato font-bold text-[18px]" to="/events">
-								Events
-							</Link>
-						</li>
-						<li className="ml-12">
-							<Link className="font-lato font-bold text-[18px]" to="/news">
-								Berichte
-							</Link>
-						</li>
-						<li className="ml-12">
-							<Link className="font-lato font-bold text-[18px]" to="/members">
-								Mitglied werden
-							</Link>
-						</li>
-						<li className="ml-12">
-							<a
-								href="https://eifelverein-rheinbach.de/"
-								target="blank"
-								className="font-lato font-bold text-[18px]"
-							>
-								Eifelverein
-							</a>
-						</li>
-					</ul>
-				</nav>
+				<div class="space-y-2 block lg:hidden">
+					<div class="w-8 h-0.5 bg-gray-600"></div>
+					<div class="w-8 h-0.5 bg-gray-600"></div>
+					<div class="w-8 h-0.5 bg-gray-600"></div>
+				</div>
+				<div className="hidden lg:flex items-center justify-between">
+					<nav>
+						<ul className="flex items-center justify-between h-full">
+							<li className="ml-12">
+								<Link className="font-lato font-bold text-[18px]" to="/about">
+									Über Uns
+								</Link>
+							</li>
+							<li className="ml-12">
+								<Link className="font-lato font-bold text-[18px]" to="/events">
+									Events
+								</Link>
+							</li>
+							<li className="ml-12">
+								<Link className="font-lato font-bold text-[18px]" to="/news">
+									Berichte
+								</Link>
+							</li>
+							<li className="ml-12">
+								<Link className="font-lato font-bold text-[18px]" to="/members">
+									Mitglied werden
+								</Link>
+							</li>
+							<li className="ml-12">
+								<a
+									href="https://eifelverein-rheinbach.de/"
+									target="blank"
+									className="font-lato font-bold text-[18px]"
+								>
+									Eifelverein
+								</a>
+							</li>
+						</ul>
+					</nav>
+
+					<a
+						href="/login"
+						className="flex justify-center items-center justify-self-end font-bold rounded-full bg-green-700 text-white py-[12px] px-[70px] mr-[16px]"
+					>
+						Login
+					</a>
+				</div>
 			</div>
-			<a
-				href="/login"
-				className="flex justify-center items-center justify-self-end font-bold rounded-full bg-green-700 text-white py-[12px] px-[70px] mr-[16px]"
-			>
-				Login
-			</a>
 		</header>
 	);
 };
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,19 +1,9 @@
+import Stage from "../components/Stage/Stage";
+
 export default function Home() {
 	return (
 		<main>
-			<div className="bg-[url('../images/bg.png')] bg-cover bg-no-repeat bg-center w-full h-screen relative overflow-hidden">
-				<div className="relative w-11/12 h-full left-2/4 -translate-x-1/2">
-					<div className="absolute bottom-20 w-full">
-						<h1 className="font-lato font-bold text-[60px] text-[#67B31F]">
-							WIER
-						</h1>
-						<p className="font-lato font-bold text-[50px] text-[#EAEAEA] ml-12">
-							sind mehr als nur wandern
-						</p>
-					</div>
-					<div className="absolute w-[300px] h-[300px] bg-[url('../images/gruppenfoto.png')] bg-cover bg-no-repeat bg-center right-0 rounded-full bottom-24"></div>
-				</div>
-			</div>
+			<Stage />
 		</main>
 	);
 }
