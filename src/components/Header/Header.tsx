c4f269c3412584246849f4b5f4591c115257272e
TobiasLueger <tobias.lueger98@gmail.com>
2022-06-01 18:28:05 +0200

    [TASK] fix nav state with help of durrinator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,6 +9,7 @@ const Header = ({}) => {
 	const [isActive, setActive] = useState(false);
 
 	const toggleClass = (state = undefined) => {
+		console.log("isActive", state, isActive);
 		setActive(state === undefined ? !isActive : state);
 	};
 
@@ -38,11 +39,9 @@ const Header = ({}) => {
 	//console.log(data);
 	// const data = getData;
 
-	//TODO: fix onclick navigation closing not working (state)
-
 	return (
-		<header className="bg-white h-24  text-black flex items-center left-2/4 -translate-x-1/2 fixed w-11/12 rounded-b-3xl rounded-br-3xl justify-between p-[15px] z-[100]">
-			<div className="flex items-center justify-between w-full">
+		<header className="bg-white h-24  text-black flex items-center left-2/4 -translate-x-1/2 fixed w-11/12 rounded-b-3xl rounded-br-3xl p-[15px] z-[100]">
+			<div className="flex items-center w-full justify-between lg:justify-start">
 				<a href="/" className="flex items-center h-full mr-[50px]">
 					<img
 						src="https://jugend.eifel-53359.de/www/img/wier-logo.jpeg"
@@ -50,7 +49,10 @@ const Header = ({}) => {
 						className="w-[100px]"
 					/>
 				</a>
-				<div className="space-y-2 block lg:hidden" onClick={toggleClass}>
+				<div
+					className="space-y-2 block lg:hidden cursor-pointer"
+					onClick={() => toggleClass()}
+				>
 					{isActive ? (
 						<X size={40} color="#67B31F" weight="bold" />
 					) : (
@@ -61,10 +63,10 @@ const Header = ({}) => {
 					className={
 						isActive
 							? "block fixed h-screen bg-white pt-24 top-0 -z-[1] -left-[32px] navi-w flex justify-center"
-							: "hidden lg:flex items-center justify-between"
+							: "hidden lg:flex lg:w-full items-center justify-between"
 					}
 				>
-					<div className="h-fit top-[20%] relative lg:flex felx-row">
+					<div className="h-fit top-[20%] relative lg:flex flex-row lg:w-full lg:justify-between">
 						<nav>
 							<ul
 								className={
@@ -77,7 +79,7 @@ const Header = ({}) => {
 									<Link
 										className="font-lato font-bold text-[20px] lg:text-[18px]"
 										to="/about"
-										onClick={toggleClass}
+										onClick={() => toggleClass()}
 									>
 										Über Uns
 									</Link>
@@ -86,7 +88,7 @@ const Header = ({}) => {
 									<Link
 										className="font-lato font-bold text-[20px] lg:text-[18px]"
 										to="/events"
-										onClick={toggleClass}
+										onClick={() => toggleClass()}
 									>
 										Events
 									</Link>
@@ -95,7 +97,7 @@ const Header = ({}) => {
 									<Link
 										className="font-lato font-bold text-[20px] lg:text-[18px]"
 										to="/news"
-										onClick={toggleClass}
+										onClick={() => toggleClass()}
 									>
 										Berichte
 									</Link>
@@ -104,7 +106,7 @@ const Header = ({}) => {
 									<Link
 										className="font-lato font-bold text-[20px] lg:text-[18px]"
 										to="/members"
-										onClick={toggleClass}
+										onClick={() => toggleClass()}
 									>
 										Mitglied werden
 									</Link>
@@ -124,7 +126,7 @@ const Header = ({}) => {
 						<a
 							href="/login"
 							className="flex justify-center items-center justify-self-end font-bold rounded-full bg-green-700 text-white py-[12px] px-[70px] lg:mr-[16px] lg:ml-24"
-							onClick={toggleClass}
+							onClick={() => toggleClass()}
 						>
 							Login
 						</a>
