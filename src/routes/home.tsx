31388602ea8576006f2b16aeeec6b4f3e39c3d7d
tobias.lueger <tobias.lueger@brandung.de>
2022-06-18 13:26:28 +0200

    [WIP] add teaser element

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,9 +1,13 @@
 import Stage from "../components/Stage/Stage";
+import Teaser from "../components/Teaser/Teaser";
 
 export default function Home() {
 	return (
-		<main>
+		<>
 			<Stage />
-		</main>
+			<main className="left-2/4 -translate-x-1/2 relative w-11/12 py-8">
+				<Teaser />
+			</main>
+		</>
 	);
 }
