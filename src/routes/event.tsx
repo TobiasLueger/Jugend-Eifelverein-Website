7f4db264d2d2fa523c2a0f791a6dab9be6fdafe1
TobiasLueger <tobias.lueger98@gmail.com>
2022-07-24 15:41:52 +0200

    [TASK] add contact form

--- src/routes/event.tsx
+++ src/routes/event.tsx
@@ -1,8 +1,12 @@
 import useEventData from "../lib/getData";
 import { useParams } from "react-router";
 import Button from "../components/Button/Button";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import ReCAPTCHA from "react-google-recaptcha";
+import { CircleNotch, X } from "phosphor-react";
 
-// TODO: MAKE EVENT DETAIL PAGE
+// TODO: MAKE EVENT DETAIL PAGE AND CLEAN UP
 
 export default function Event(props: any) {
 	const params = useParams();
@@ -22,6 +26,47 @@ export default function Event(props: any) {
 
 	!loading ? (data = eventData[0]) : "";
 
+	const form: any = useRef();
+
+	const [mailConfirm, setMailConfirm] = useState(false);
+	const [mailFormOpen, setMailFormOpen] = useState(false);
+	const [emailLoading, setEmailLoading] = useState(false);
+
+	function onClose() {
+		setMailFormOpen(!mailFormOpen);
+		setMailConfirm(false);
+	}
+
+	function onChange(value: any) {
+		console.log("Captcha value:", value);
+	}
+
+	const sendEmail = (e: any) => {
+		e.preventDefault();
+
+		console.log("form", form);
+
+		setEmailLoading(true);
+
+		emailjs
+			.sendForm(
+				"service_0eip2u7",
+				"template_to0r83j",
+				form.current,
+				"cFxR8Hw-2ziVbOuxS"
+			)
+			.then(
+				(result: any) => {
+					console.log(result.text);
+					setMailConfirm(true);
+					setEmailLoading(false);
+				},
+				(error: any) => {
+					console.log(error.text);
+				}
+			);
+	};
+
 	return (
 		<>
 			<main>
@@ -38,11 +83,12 @@ export default function Event(props: any) {
 
 						<div className="bg-[#133a4a] relative w-screen -left-[4.5%] text-white flex flex-col lg:flex-row justify-between items-center py-[31px] px-[15px] lg:px-[50px] text-[20px] lg:text-[22px]">
 							<div className="mb-5 lg:mb-0">{data.acf.startdatum}</div>
-							<Button
-								href={`mailto:${data.acf.anmeldung}?subject=Anmeldung zu: ${data.title.rendered}&body=Name:</br>Vorname:`}
-								title="Anmelden"
-								className="w-[100%] lg:w-fit"
-							/>
+							<button
+								className="w-[100%] lg:w-fit font-bold flex justify-center items-center rounded-xl bg-green-700 w-fit text-white py-[12px] px-[70px]"
+								onClick={onClose}
+							>
+								Anmelden
+							</button>
 						</div>
 
 						<section>
@@ -64,6 +110,74 @@ export default function Event(props: any) {
 					</>
 				)}
 			</main>
+			{!loading && (
+				<div
+					className={`${
+						mailFormOpen ? "" : "hidden"
+					} fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white z-50 p-5 rounded-[6px] border-[#133849] border-[1px] min-h-[300px] min-w-[300px] flex justify-center items-center`}
+				>
+					{!mailConfirm && !emailLoading && (
+						<form ref={form} onSubmit={sendEmail} className="flex flex-col">
+							<label>Vor/Nachname</label>
+							<input type="text" name="from_name" required />
+							<label>Email</label>
+							<input type="email" name="from_email" required />
+							<label>Willst du noch mehr wissen?</label>
+							<textarea name="message" />
+
+							<input
+								className="hidden"
+								type="text"
+								name="event"
+								value={`${data.title.rendered}`}
+							/>
+							<input
+								className="hidden"
+								type="text"
+								name="to_name"
+								value={`${data.acf.leitung}`}
+							/>
+							<input
+								className="hidden"
+								type="text"
+								name="send_to"
+								value={`${data.acf.anmeldung}`}
+							/>
+
+							<ReCAPTCHA
+								sitekey="6LftDBghAAAAAA1OJ-nX7Ny_Y74Hzw3V-joqce2K"
+								onChange={onChange}
+							/>
+
+							<input type="submit" value="Send" className="btn" />
+						</form>
+					)}
+					{emailLoading && (
+						<>
+							<span>Anmeldung wird versendet: </span>
+							<CircleNotch
+								size={40}
+								className="animate-spin"
+								color="#133849"
+								weight="bold"
+							/>
+						</>
+					)}
+					{mailConfirm && (
+						<div className="w-full text-center">
+							Danke für deine Anmeldung. Wir melden uns bei dir mit weiteren
+							Infos.
+						</div>
+					)}
+					<X
+						size={20}
+						color="#67B31F"
+						weight="bold"
+						className="absolute top-3 right-3 cursor-pointer"
+						onClick={onClose}
+					/>
+				</div>
+			)}
 		</>
 	);
 }
