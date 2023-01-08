a1575cf532347064966eb0178cfecc94aec7c335
TobiasLueger <tobias.lueger98@gmail.com>
2023-01-08 13:40:31 +0100

    [WIP] work on event page

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -1,20 +1,30 @@
 export default function Pill({
 	bookedUp,
 	freePlaces,
+	customeClass,
 }: {
 	bookedUp?: boolean;
 	freePlaces?: number;
+	customeClass?: string;
 }) {
 	return (
 		<>
 			{bookedUp ? (
-				<div className="flex w-fit items-center justify-center rounded-full bg-white text-red text-[13px] font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]">
+				<div
+					className={`flex w-fit items-center justify-center rounded-full bg-white text-red ${
+						customeClass ? customeClass : "text-[13px]"
+					} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}
+				>
 					AUSGEBUCHT
 				</div>
 			) : null}
 
 			{freePlaces ? (
-				<div className="flex w-fit items-center justify-center rounded-full bg-white text-orange text-[13px] font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]">
+				<div
+					className={`${customeClass} flex w-fit items-center justify-center rounded-full bg-white text-orange ${
+						customeClass ? customeClass : "text-[13px]"
+					} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}
+				>
 					🔥 NOCH {freePlaces} PLÄTZE FREI
 				</div>
 			) : null}
--- src/routes/event.jsx
+++ src/routes/event.jsx
@@ -9,6 +9,8 @@ import {
 	UsersThree,
 	ClipboardText,
 	Calendar,
+	BagSimple,
+	UserCircle,
 } from "phosphor-react";
 import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
@@ -17,19 +19,11 @@ import {
 	EMAILJS_TEMPLATE_ID,
 	EMAILJS_PUBLIC_KEY,
 } from "../lib/Env";
+import Pill from "../components/Pill/Pill";
 
 // TODO: MAKE EVENT DETAIL PAGE
-/* export default function Event(props: any) { */
 export default function Event() {
 	const params = useParams();
-
-	/* const {
-		eventData,
-		loading,
-		error,
-	}: { eventData: any, loading: boolean, error: boolean } = useEventData(
-		`events?slug=${params.id}`
-	); */
 
 	const { slugData, loading, error } = useSlugData(`events?slug=${params.id}`);
 
@@ -82,98 +76,124 @@ export default function Event() {
 
 	return (
 		<>
-			<main className="mt-48">
+			<main className="mt-24">
 				{!loading && (
 					<>
-						<div className="flex relative gap-4">
-							<div className="w-2/3 max-h-60">
+						<div className="flex flex-wrap">
+							<div className="w-full max-w-[100%] basis-[100%] lg:max-w-[50%] lg:basis-[50%] grow-0 shrink-0 pr-8 mb-8 lg:mb-0">
 								<h2 className=" leading-snug tracking-normal overflow-hidden overflow-ellipsis box">
 									{data.title.rendered}
 								</h2>
+								<p className="mt-3">{data.acf.fur_wen}</p>
+								<p
+									className="mt-3"
+									dangerouslySetInnerHTML={{ __html: data.acf.text }}
+								></p>
+								<button
+									title="Anmelden"
+									className="btn w-[100%] lg:w-fit mt-6"
+									onClick={toggleEmailForm}
+									disabled={data.acf.ausgebucht ? true : false}
+								>
+									Anmelden
+								</button>
 							</div>
-							<div className="w-1/2 flex justify-center">
-								<div className="overflow-hidden rounded-[12px] aspect-[1/1] shadow-xl w-full flex max-h-96">
+							<div className="w-full max-w-[100%] basis-[100%] lg:max-w-[50%] lg:basis-[50%] grow-0 shrink-0">
+								<div className="overflow-hidden rounded-[12px] shadow-xl w-full flex relative">
 									<img
 										src={data.acf.bild}
-										className="w-full object-cover"
+										className="w-full object-cover h-full max-h-[500px]"
 										alt=""
 									/>
+									{(data.acf.ausgebucht || data.acf.freie_plaetze) && (
+										<div className="absolute top-[10px] lg:top-[15px] right-[10px] lg:right-[15px]">
+											<Pill
+												bookedUp={data.acf.ausgebucht}
+												freePlaces={data.acf.freie_plaetze}
+												customeClass="text-[16px]"
+											/>
+										</div>
+									)}
 								</div>
 							</div>
-							<div className="absolute  w-full h-fit bottom-8 flex flex-row justify-between">
-								<div className="flex flex-wrap w-2/3 bg-white shadow-2xl rounded-[12px] p-4">
-									<div className="flex flex-col w-1/2 justify-between">
-										<div className="mb-5 lg:mb-0 ">
-											{data.acf.startdatum && (
-												<p className="flex gap-2 text-[20px] font-bold text-greyDark">
-													<Calendar
-														size={30}
-														className="text-greyDark"
-														weight="bold"
-													/>
-													{data.acf.startdatum}
-												</p>
-											)}
-										</div>
-										<div className="mb-5 lg:mb-0 flex">
-											{data.acf.startzeit && (
-												<p className="flex gap-2 text-[20px] font-bold text-greyDark">
-													<Clock
-														size={30}
-														className="text-greyDark"
-														weight="bold"
-													/>
-													{data.acf.startzeit} Uhr
-												</p>
-											)}
-										</div>
+						</div>
+
+						<div className="w-full h-fit mt-8 flex flex-row justify-between">
+							<div className="flex flex-wrap w-full lg:w-2/3 bg-white shadow-2xl rounded-[12px] p-4">
+								<div className="flex flex-col w-1/2 justify-between">
+									<div>
+										<p className="text-[20px] font-bold text-greyDark mb-2">
+											Vom:
+										</p>
 									</div>
-									<div className="flex flex-col w-1/2 justify-between">
-										<div className="mb-5 lg:mb-0">
-											{data.acf.enddatum && (
-												<p className="flex gap-2 text-[20px] font-bold text-greyDark">
-													<Calendar
-														size={30}
-														className="text-greyDark"
-														weight="bold"
-													/>
-													{data.acf.enddatum}
-												</p>
-											)}
-										</div>
-										<div className="mb-5 lg:mb-0">
-											{data.acf.endzeit && (
-												<p className="flex gap-2 text-[20px] font-bold text-greyDark">
-													<Clock
-														size={30}
-														className="text-greyDark"
-														weight="bold"
-													/>
-													{data.acf.endzeit} Uhr
-												</p>
-											)}
-										</div>
+									<div className="mb-0 ">
+										{data.acf.datum && (
+											<p className="flex gap-2 text-[20px] font-bold text-greyDark">
+												<Calendar
+													size={30}
+													className="text-greyDark"
+													weight="bold"
+												/>
+												{new Date(data.acf.datum).toLocaleString("de-DE", {
+													day: "numeric",
+													month: "short",
+													year: "numeric",
+												})}
+											</p>
+										)}
+									</div>
+									<div className="mb-5 lg:mb-0 flex">
+										{data.acf.startzeit && (
+											<p className="flex gap-2 text-[20px] font-bold text-greyDark">
+												<Clock
+													size={30}
+													className="text-greyDark"
+													weight="bold"
+												/>
+												{data.acf.startzeit} Uhr
+											</p>
+										)}
 									</div>
 								</div>
-								<div className=" bg-white p-8 z-50 shadow-2xl rounded-[12px]">
-									<a
-										title="Anmelden"
-										className="btn w-[100%] lg:w-fit"
-										onClick={toggleEmailForm}
-									>
-										Anmelden
-									</a>
+								<div className="flex flex-col w-1/2 justify-between">
+									<div>
+										<p className="text-[20px] font-bold text-greyDark mb-2">
+											{(data.acf.enddatum || data.acf.endzeit) && "Bis:"}
+										</p>
+									</div>
+									<div className="mb-0">
+										{data.acf.enddatum && (
+											<p className="flex gap-2 text-[20px] font-bold text-greyDark">
+												<Calendar
+													size={30}
+													className="text-greyDark"
+													weight="bold"
+												/>
+												{new Date(data.acf.enddatum).toLocaleString("de-DE", {
+													day: "numeric",
+													month: "short",
+													year: "numeric",
+												})}
+											</p>
+										)}
+									</div>
+									<div className="mb-5 lg:mb-0">
+										{data.acf.endzeit && (
+											<p className="flex gap-2 text-[20px] font-bold text-greyDark">
+												<Clock
+													size={30}
+													className="text-greyDark"
+													weight="bold"
+												/>
+												{data.acf.endzeit} Uhr
+											</p>
+										)}
+									</div>
 								</div>
 							</div>
 						</div>
 
 						<section>
-							<p>{data.acf.fur_wen}</p>
-							<p
-								className=""
-								dangerouslySetInnerHTML={{ __html: data.acf.text }}
-							></p>
-
 							{data.acf.enthaltene_leistungen && (
 								<p className="flex">
 									<UsersThree
@@ -230,6 +250,26 @@ export default function Event() {
 								<p className="flex">
 									<Clock size={25} className="text-greyDark" weight="bold" />
 									{data.acf.endzeit} Uhr
+								</p>
+							)}
+							{data.acf.mitbringen && (
+								<p className="flex">
+									<BagSimple
+										size={25}
+										className="text-greyDark"
+										weight="bold"
+									/>
+									{data.acf.mitbringen}
+								</p>
+							)}
+							{data.acf.leitung && (
+								<p className="flex">
+									<UserCircle
+										size={25}
+										className="text-greyDark"
+										weight="bold"
+									/>
+									{data.acf.leitung}
 								</p>
 							)}
 						</section>
