fb1af8f6df0106e7c79e572bdc1f9bc96685cf15
TobiasLueger <tobias.lueger98@gmail.com>
2023-04-18 12:12:29 +0200

    [TASK] add wartelisten pill and add text on event for wartelisten info

--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -1,33 +1,11 @@
-export default function Pill({
-	bookedUp,
-	freePlaces,
-	customeClass,
-}: {
-	bookedUp?: boolean;
-	freePlaces?: number;
-	customeClass?: string;
-}) {
+export default function Pill({ bookedUp, waitlist, freePlaces, customeClass }: { bookedUp?: boolean; waitlist?: boolean; freePlaces?: number; customeClass?: string }) {
 	return (
 		<>
-			{bookedUp ? (
-				<div
-					className={`flex w-fit items-center justify-center rounded-full bg-white text-red ${
-						customeClass ? customeClass : "text-[13px]"
-					} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}
-				>
-					AUSGEBUCHT
-				</div>
-			) : null}
+			{bookedUp ? <div className={`flex w-fit items-center justify-center rounded-full bg-white text-red ${customeClass ? customeClass : "text-[13px]"} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}>AUSGEBUCHT</div> : null}
 
-			{freePlaces ? (
-				<div
-					className={`${customeClass} flex w-fit items-center justify-center rounded-full bg-white text-orange ${
-						customeClass ? customeClass : "text-[13px]"
-					} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}
-				>
-					🔥 NOCH {freePlaces} PLÄTZE FREI
-				</div>
-			) : null}
+			{waitlist ? <div className={`flex w-fit items-center justify-center rounded-full bg-white text-orange ${customeClass ? customeClass : "text-[13px]"} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}>⏳ WARTELISTE VERFÜGBAR</div> : null}
+
+			{freePlaces ? <div className={`${customeClass} flex w-fit items-center justify-center rounded-full bg-white text-orange ${customeClass ? customeClass : "text-[13px]"} font-bold px-[12px] lg:px-[20px] py-[4px] lg:py-[5px]`}>🔥 NOCH {freePlaces} PLÄTZE FREI</div> : null}
 		</>
 	);
 }
--- src/routes/event.jsx
+++ src/routes/event.jsx
@@ -154,11 +154,12 @@ export default function Event() {
 								</div>
 								<p className="mt-3">{data.acf.fur_wen}</p>
 								<p className="mt-3" dangerouslySetInnerHTML={{ __html: data.acf.text }}></p>
+								<p className={`text-red ${data.acf.ausgebucht ? "mt-5 before:content-['❗'] flex" : "!hidden"}`}>Leider ist diese Veranstaltung derzeit ausgebucht. Trage Dich gerne auf unserer Warteliste ein. Wir informieren Dich, sobald ein Platz frei wird.</p>
 								<div className="flex gap-5 flex-wrap">
-									<button title="Anmelden" className="btn w-[100%] lg:w-fit mt-6" onClick={toggleEmailForm} disabled={data.acf.ausgebucht ? true : false}>
+									<button title="Anmelden" className="btn !w-[100%] lg:!w-fit mt-6" onClick={toggleEmailForm} disabled={data.acf.ausgebucht ? true : false}>
 										Anmelden
 									</button>
-									<button title="Warteliste" className={`btn w-[100%] lg:w-fit mt-6 ${data.acf.ausgebucht ? "" : "!hidden"}`} onClick={toggleWaitlistForm}>
+									<button title="Warteliste" className={`btn !w-[100%] lg:!w-fit mt-4 lg:mt-6 ${data.acf.ausgebucht ? "" : "!hidden"}`} onClick={toggleWaitlistForm}>
 										Zur Warteliste
 									</button>
 								</div>
