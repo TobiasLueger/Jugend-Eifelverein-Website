d94ac023a7ab0f64664b7d1abd4cf8527606e0e1
tobias.lueger <tobias.lueger@brandung.de>
2023-11-08 12:46:27 +0100

    [BUG] remove speed insights vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-// import reportWebVitals from './reportWebVitals';
-import { webVitals } from '../src/lib/vitals';
 
 import ScrollToTop from "./lib/ScrollToTop.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,5 +11,3 @@ root.render(
 		<App />
 	</BrowserRouter>
 );
-
-webVitals
