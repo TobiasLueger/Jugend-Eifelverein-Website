f7b6962a3bfd698156563b44ce609ea0c9ecab56
Tobias Lüger <tobias-lueger@web.de>
2024-08-31 13:51:23 +0200

    test process env

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -5,7 +5,7 @@ import { INTERN_PASSWORD } from "../lib/Env";
 export default function Login() {
 	let internPassword: any;
 
-	console.log(process.env)
+	console.log("ENVS:",process.env)
 
 	if (process.env.NODE_ENV === "production") {
 		// For production
