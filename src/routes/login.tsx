e1143210f04ecaeea3f21f3ea902ead28449170a
Tobias Lüger <tobias-lueger@web.de>
2024-08-31 13:53:59 +0200

    test process env

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -5,8 +5,6 @@ import { INTERN_PASSWORD } from "../lib/Env";
 export default function Login() {
 	let internPassword: any;
 
-	console.log("ENVS:",process.env)
-
 	if (process.env.NODE_ENV === "production") {
 		// For production
 		internPassword = process.env.VERCEL_INTERN_PASSWORD;
