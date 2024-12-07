650a10acae2e9c6832f7a02d0be1df9873de0164
Tobias Lüger <44581179+TobiasLueger@users.noreply.github.com>
2024-12-07 13:55:17 +0100

    Update events.tsx

--- src/routes/events.tsx
+++ src/routes/events.tsx
@@ -5,7 +5,7 @@ export default function Events() {
 		<main className="pt-[116px]">
 			<h2>Veranstaltungen</h2>
 			<p className="mt-2 lg:pl-2">
-				Hier siehst du alle unsere tollen Veranstaltungen. Sie dich um und such
+				Hier siehst du alle unsere tollen Veranstaltungen. Sieh dich um und such
 				dir eine aus:
 			</p>
 			<EventList home={false} />
