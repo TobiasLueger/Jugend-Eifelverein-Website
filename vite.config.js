8b65cafa38740db9d4e48c875bc665c63f5e8944
Tobias Lüger <tobias-lueger@web.de>
2025-09-10 21:49:11 +0200

    add million lint

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [MillionLint.vite({
+		auto: true,
+		ignore: ["node_modules"],
+	}), react()],
 	server: {
 		host: true,
 	},
